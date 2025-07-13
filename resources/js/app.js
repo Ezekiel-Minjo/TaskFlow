f1d867acd76e6b84dab5ceaaa22c1a21057da9d5
minjo <ezekielminjo4@gmail.com>
2025-07-13 14:37:52 +0300

    Initial Laravel setup with Breeze and Bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,22 @@
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
 import '../css/app.css';
+import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { createInertiaApp } from '@inertiajs/vue3';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createApp, h } from 'vue';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    resolve: (name) => import(`./Pages/${name}.vue`),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .mount(el);
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', {eager: true});
+        return pages[`./Pages/${name}.vue`];
     },
+    setup({el, App, props, plugin}) {
+        createApp({ render: () => h(App, props)})
+        .use(plugin)
+        .mount(el);
+    }
 });
